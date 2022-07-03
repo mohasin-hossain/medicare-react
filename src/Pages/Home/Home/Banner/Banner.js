1b6d834d99a9248a74ccec05f9f6b20d04171588
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2022-07-03 10:54:00 +0600

    Banner

--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -1,9 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>This is banner!</h1>
+        <div id='banner'>
+            <h1>THE <span>RIGHT</span> <br /> PEDIATRICIAN</h1>
+            <p>We at MediCare are always fully focused on helping <br /> your child and you to overcame any hurdle or any other problem.</p>
+           <div className="btns">
+                <button>More details</button>
+                <button>Book appointment <FontAwesomeIcon className='right-icon' icon={faArrowRight} /></button>
+           </div>
         </div>
     );
 };
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Header.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../../images/surgery-logo.png";
+
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
-    );
+  return (
+    <header>
+      <Navbar bg="light" expand="xl" fixed="top">
+        <Container fluid className="bg-light">
+          <img className="logo" src={logo} alt="" />
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="ms-auto my-4 my-xl-0 main-links" navbarScroll>
+              <Link to="/home">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/services">Services</Link>
+              <Link to="/faq">Faq</Link>
+              <Link to="/contact">Contact</Link>
+            </Nav>
+            <div className="login-section">
+              <span>
+                <Link to="/login">Login</Link>
+              </span>
+              <span>
+                <small style={{ marginRight: ".5rem" }}>/</small>
+                <Link to="/register">Register</Link>
+              </span>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
