59924e41fcb8c5ade3344fccb7e6754ae094ce3e
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2022-07-03 11:13:38 +0600

    Banner retouch

--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -6,8 +6,8 @@ import './Banner.css';
 const Banner = () => {
     return (
         <div id='banner'>
-            <h1>THE <span>RIGHT</span> <br /> PEDIATRICIAN</h1>
-            <p>We at MediCare are always fully focused on helping <br /> your child and you to overcame any hurdle or any other problem.</p>
+            <h1>THE <span>RIGHT</span> PEDIATRICIAN</h1>
+            <p style={{maxWidth: "60%", textAlign: "center"}}>We at MediCare are always fully focused on helping your child and you to overcame any hurdle or any other problem.</p>
            <div className="btns">
                 <button>More details</button>
                 <button>Book appointment <FontAwesomeIcon className='right-icon' icon={faArrowRight} /></button>
