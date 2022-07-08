66133b7356897b477639fe6969b1703e4b72e109
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2022-07-08 11:40:33 +0600

    NotFound page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import notFoundImg from '../../images/404.png';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Sorry! The requested page is not available!</h1>
+        <div className='not-found'>
+            <img src={notFoundImg} alt="" />
         </div>
     );
 };
